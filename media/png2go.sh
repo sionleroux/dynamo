2fd907fe010a6487f4ae0ae9c4e905653bf017c7
Siôn le Roux <sinisterstuf@gmail.com>
2021-02-07 21:09:26 +0100

    Finish conversion script
    It now generates full Go code with boilerplate based on a provided PNG
    filename for input.  I also fixed a bug in the matching, 67 and 199 were
    too general, they didn't just match against the numbers but also against
    the line numbers, resulting in a black line of zeroes along column 67.

--- media/png2go.sh
+++ media/png2go.sh
@@ -1,1 +1,27 @@
-cat 1.txt | tail -n +2 | sed -e '/67/s/.*/0/' -e '/199/s/.*/1/' | tr '\n' ', ' > title.txt
+#!/bin/bash
+
+filename=$1
+
+if [[ -z $1 ]]; then
+    echo 'argument 1 must be input filename' >&2
+    exit 1
+fi
+
+name=$(echo $filename | sed -e 's/.png//' -e 's/^./\U&/')
+goname=${filename%%.png}.go
+
+echo -ne "package media
+
+// $name is generated from $filename
+var $name []uint8 = []uint8{\n\t" > $goname
+
+# skip the first metadata line
+# convert colours to 0s & 1s
+# replace newlines with commas
+convert title.png txt:- \
+    | tail -n +2 \
+    | sed -e '/43523D/s/.*/0/' -e '/C7F0D8/s/.*/1/' \
+    | tr '\n' ', ' \
+    >> $goname
+
+echo -e '\n}' >> $goname
