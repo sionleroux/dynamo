e0bafd2359f2afe3cda607d3031e453649434e2b
Siôn le Roux <sinisterstuf@gmail.com>
2021-02-07 13:27:55 +0100

    Add comments and minor tweaks to appease the Go linter

--- main.go
+++ main.go
@@ -15,12 +15,17 @@ import (
 )
 
 var (
-	colorLight   color.Color   = color.RGBA{199, 240, 216, 255}
-	colorDark    color.Color   = color.RGBA{67, 82, 61, 255}
-	nokiaPalette color.Palette = color.Palette{colorDark, colorLight}
-	Levels       []int         = []int{10, 6, 4, 3, 2}
+	// ColorLight is the ON or 1 screen colour, similar to white
+	ColorLight color.Color = color.RGBA{199, 240, 216, 255}
+	// ColorDark is the OFF or 0 screen colour, similar to black
+	ColorDark color.Color = color.RGBA{67, 82, 61, 255}
+	// NokiaPalette is a 1-bit palette of greenish colours simulating Nokia 3310
+	NokiaPalette color.Palette = color.Palette{ColorDark, ColorLight}
+	// Levels maps level difficulty indices to maze size scaling factors
+	Levels []int = []int{10, 6, 4, 3, 2}
 )
 
+// Levels represent the difficulty of different game levels
 const (
 	LevelBeginner int = iota
 	LevelEasy
@@ -51,7 +56,7 @@ func main() {
 
 	go func() {
 		blinker := time.NewTicker(500 * time.Millisecond)
-		for _ = range blinker.C {
+		for range blinker.C {
 			game.BlinkOn = !game.BlinkOn
 		}
 	}()
@@ -72,7 +77,7 @@ type Game struct {
 	Source  rand.Source
 }
 
-// Update updates a game by one tick. The given argument represents a screen image.
+// Update updates a game by one tick.
 func (g *Game) Update() error {
 	// Pressing Esc any time quits immediately
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
@@ -118,7 +123,7 @@ func (g *Game) Update() error {
 
 // Draw draws the game screen by one frame
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(colorDark)
+	screen.Fill(ColorDark)
 	if g.Player.TorchOn {
 		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Translate(
@@ -132,22 +137,25 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			float64(g.Maze.Exit.Y+g.Maze.Offset.Y),
 			float64(g.Maze.Exit.X+g.Maze.Offset.X+1),
 			float64(screen.Bounds().Max.Y),
-			colorLight,
+			ColorLight,
 		)
 	}
-	playercolor := colorDark
+	playercolor := ColorDark
 	if g.BlinkOn || !g.Player.TorchOn {
-		playercolor = colorLight
+		playercolor = ColorLight
 	}
 	playerPos := g.Player.Coords.Add(g.Maze.Offset)
 	screen.Set(playerPos.X, playerPos.Y, playercolor)
 }
 
-// Layout is hardcoded for now, may be made dynamic in future
+// Layout scales the pixels when the windows is resized
+// This means that in a bigger window all the pixels will become bigger squares
 func (g *Game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, screenHeight int) {
 	return g.Size.X, g.Size.Y
 }
 
+// NextLevel sets up the next level of the game
+// It handles things like increasing difficulty and resetting the Player state
 func (g *Game) NextLevel() {
 	g.Win = false
 	g.Player = NewPlayer()
@@ -157,7 +165,7 @@ func (g *Game) NextLevel() {
 	g.Maze = NewMaze(g.Source, g.Level, g.Size)
 }
 
-// Player is the pixel the player controlers
+// Player is the pixel the player controls
 type Player struct {
 	Coords  image.Point
 	TorchOn bool
@@ -165,14 +173,21 @@ type Player struct {
 	Moved   bool
 }
 
+// NewPlayer initialises a new Player object with default values
 func NewPlayer() *Player {
 	return &Player{
-		Coords:  image.Pt(1, 1),
-		TorchOn: true,
+		Coords:  image.Pt(1, 1), // This is inset by 1 because 0,0 is a wall
+		TorchOn: true,           // Start with torch on so that the map is shown
 	}
 }
 
+// Move moves the Player in the given direction if possible
+// This includes checks for whether the move is legal at all, e.g. would collide
+// with a wall.  It also includes special logic for movement speed when the key
+// is being tapped and when it's being held down.
 func (p *Player) Move(maze *Maze, dest image.Point, key ebiten.Key) {
+
+	// Still cooling down from last move, unless the key was tapped
 	if p.Step > 0 && !inpututil.IsKeyJustPressed(key) {
 		return
 	}
@@ -181,29 +196,36 @@ func (p *Player) Move(maze *Maze, dest image.Point, key ebiten.Key) {
 	if !p.Moved {
 		if inpututil.IsKeyJustPressed(key) {
 			p.Moved = true
-		} else {
+		} else { // Skip return so that lower-down "just pressed" logic runs
 			return
 		}
 	}
 
+	// Even just attempting to move turns off the torch
 	p.TorchOn = false
+
+	// Do the actual move if legal
 	newCoords := p.Coords.Add(dest)
-	if maze.Image.At(newCoords.X, newCoords.Y) != colorDark {
+	if maze.Image.At(newCoords.X, newCoords.Y) != ColorDark {
 		p.Coords = newCoords
-		p.Step = 2
+		p.Step = 2 // short cooldown when holding down
 		if inpututil.IsKeyJustPressed(key) {
-			p.Step = 15
+			p.Step = 15 // long first cooldown when tapping key
 		}
 	}
 }
 
+// Maze contains all information about mazes
+// Not just the generated maze image but also any other meta-data that can be
+// used for interacting with the maze.
 type Maze struct {
-	Image  *ebiten.Image
-	Maze   *maze.Maze
-	Exit   image.Point
-	Offset image.Point
+	Image  *ebiten.Image // Maze image in 1-bit for drawing & collision logic
+	Maze   *maze.Maze    // Original maze object for solving
+	Exit   image.Point   // The exit location, for end-game logic
+	Offset image.Point   // Used to centre the maze at draw time
 }
 
+// NewMaze generates a new maze based on difficulty level and random source
 func NewMaze(source rand.Source, level int, gameSize image.Point) *Maze {
 	mymaze := maze.WithKruskal(source).Generate(
 		gameSize.X/Levels[level]-1,
@@ -212,7 +234,7 @@ func NewMaze(source rand.Source, level int, gameSize image.Point) *Maze {
 
 	// Convert to Nokia colours
 	grayMaze := maze.Gray(mymaze)
-	colorMaze := image.NewPaletted(grayMaze.Bounds(), nokiaPalette)
+	colorMaze := image.NewPaletted(grayMaze.Bounds(), NokiaPalette)
 	for k, v := range grayMaze.Pix {
 		if v == 255 {
 			colorMaze.Pix[k] = 1
@@ -222,15 +244,15 @@ func NewMaze(source rand.Source, level int, gameSize image.Point) *Maze {
 	// Find an exit at the bottom right
 	var exit image.Point
 	for i := colorMaze.Rect.Max.X; i > 0; i-- {
-		if colorMaze.At(i, colorMaze.Rect.Max.Y-2) == colorLight {
+		if colorMaze.At(i, colorMaze.Rect.Max.Y-2) == ColorLight {
 			exit = image.Pt(i, colorMaze.Rect.Max.Y)
-			colorMaze.Set(exit.X, exit.Y-1, colorLight)
+			colorMaze.Set(exit.X, exit.Y-1, ColorLight)
 			break
 		}
 	}
-
 	mazeImage := ebiten.NewImageFromImage(colorMaze)
 
+	// Calculate offset from origin for centring on the screen
 	offset := image.Pt(
 		(gameSize.X-mazeImage.Bounds().Dx())/2,
 		(gameSize.Y-mazeImage.Bounds().Dy())/2,
